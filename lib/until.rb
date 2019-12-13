0ea497e64191afb9aeaddf7f1fb91ddeeca7491e
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-13 01:10:03 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 0
   until levitation_force == 10
     puts " #{using_until} until the levitation force is incremented to 10"
-    levitation_force += 1
+    levitation_force = levitation_force + 1
     
 end
 
